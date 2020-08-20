304dd3c85962edb0cfc7f305cf92d479dea6357a
Guilherme Costa <guidohcosta@gmail.com>
2020-08-19 22:04:49 -0300

    feat(database): mudar banco pra postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_20_002646) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "client"
     t.integer "status_cd", default: 0
